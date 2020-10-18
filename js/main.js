68f37441dd431db27261af3866c41c76bb63c37d
UltraJag214 <HAYDEN.GIBBONS8920@STU.MTEC.EDU>
2020-10-17 19:01:21 -0600

    Added function to remove the previous html added so we can add new lists

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const listsContainer = document.querySelector('[data-lists]')
+
+let lists = ['name', 'todo']
+
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.classList.add("list-name")
+        listElement.innterText = list
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
