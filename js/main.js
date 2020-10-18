a0e5ddb4ee685e2abaaaa08c3d30159970503567
UltraJag214 <HAYDEN.GIBBONS8920@STU.MTEC.EDU>
2020-10-17 19:48:15 -0600

    added local storage

--- js/main.js
+++ js/main.js
@@ -2,13 +2,10 @@ const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form')
 const newListInput = document.querySelector('[data-new-list-input')
 
-let lists = [{
-    id: 1,
-    name: 'name'
-}, {
-    id: 2,
-    name: 'todo'
-}]
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -17,11 +14,20 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
-    render()
+    saveAndRender()
 })
 
 function createList(name) {
     return { id: Date.now().toString(), name: name, tasks: [] } 
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify())
 }
 
 function render() {
@@ -31,6 +37,9 @@ function render() {
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
         listElement.innterText = list.name
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
